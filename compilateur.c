7bc753acd694ad513c95ad0905088b5c21960167
Hamza <hamzatamenaoul@gmail.com>
2019-10-28 12:40:01 +0100

    Rename README.txt to README.md

--- compilateur.c
+++ compilateur.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "global.h"
 #include "analyseur_synthaxique.h"
 
 
-int main() {
-	/* The name of the output file */
-	strcpy(output_file_name, "output");
+int main(int argc, char** argv) {
+	/* Open the file */
+	assert(argc == 2);
+	file = fopen(argv[1], "r");
 
-	file = fopen("test2.ada", "r");
+	/* The name of the output file */
+	if (argc == 3) strcpy(argv[2], "output");
+	else strcpy(output_file_name, "output");
+
+	/* Starting compilation */
 	PROGRAM();
 	fclose(file);
 	
